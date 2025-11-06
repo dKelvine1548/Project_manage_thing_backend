1307c28ac346bff73d4a359f401cb999405c1a69
DJOKOUO Kelvine <djokouokelvine@gmail.com>
2025-11-06 19:20:51 +0100

    create stuff router

---
+++ routers/stuff.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import * as stuffCtrl from '../controllers/stuff.js';
+import auth from '../middleware/auth.js';
+import multer from '../middleware/multer-config.js';
+
+const stuffRoutes = express.Router();
+
+stuffRoutes.get('/', auth,stuffCtrl.getAllThings);
+stuffRoutes.post('/', auth, multer, stuffCtrl.createThing);
+stuffRoutes.get('/:id', auth, stuffCtrl.getOneThing);
+stuffRoutes.put('/:id', auth, stuffCtrl.modifyThing);
+stuffRoutes.delete('/:id', auth, stuffCtrl.deleteThing);
+
+
+export default stuffRoutes;
